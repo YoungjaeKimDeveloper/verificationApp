eb21f51aa18e2e5bcc4d3c265ea79e36319e73e2
Youngjae <2015fromjeki@gmail.com>
2025-01-06 17:22:56 +1100

    Before creating PasswordStrengthMeter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Homepage from "../pages/Homepage";
+import FloatingShape from "./components/FloatingShape";
+
+// Pages
+import HomePage from "../pages/Homepage";
+import SignupPage from "../pages/SignupPage";
+import LoginPage from "../pages/LoginPage";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+    <div className="min-h-screen w-screen bg-gradient-to-r from-green-100 to-green-300 flex items-center justify-center">
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
